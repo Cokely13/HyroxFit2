e734027c36db1840dc6a1e52429eacbc7895b121
Ryan Cokely <ryan.cokely@gmail.com>
2024-07-02 10:32:25 -0400

    Initial Commit

---
+++ HyroxFit2/HyroxFit2App.swift
@@ -0,0 +1,17 @@
+//
+//  HyroxFit2App.swift
+//  HyroxFit2
+//
+//  Created by Ryan Cokely on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HyroxFit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
