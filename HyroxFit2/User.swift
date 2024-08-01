7ee4b3469ad9139061c9f4d6e446484d104f3e60
Ryan Cokely <ryan.cokely@gmail.com>
2024-08-01 17:39:41 -0400

    times added

--- HyroxFit2/User.swift
+++ HyroxFit2/User.swift
@@ -1,8 +1,51 @@
+////
+////  User.swift
+////  HyroxFit2
+////
+////  Created by Ryan Cokely on 7/31/24.
+////
 //
-//  User.swift
-//  HyroxFit2
+//import Foundation
 //
-//  Created by Ryan Cokely on 7/31/24.
+//struct User: Identifiable, Codable {
+//    var id = UUID()
+//    var username: String
+//    var password: String
+//}
+//
+//class UserData: ObservableObject {
+//    @Published var users: [User] = []
+//
+//    init() {
+//        loadUsers()
+//    }
+//
+//    func addUser(username: String, password: String) {
+//        let newUser = User(username: username, password: password)
+//        users.append(newUser)
+//        saveUsers()
+//    }
+//
+//    func authenticate(username: String, password: String) -> Bool {
+//        if let user = users.first(where: { $0.username == username && $0.password == password }) {
+//            return true
+//        }
+//        return false
+//    }
+//
+//    private func saveUsers() {
+//        if let encoded = try? JSONEncoder().encode(users) {
+//            UserDefaults.standard.set(encoded, forKey: "users")
+//        }
+//    }
+//
+//    private func loadUsers() {
+//        if let savedUsers = UserDefaults.standard.data(forKey: "users"),
+//           let decodedUsers = try? JSONDecoder().decode([User].self, from: savedUsers) {
+//            users = decodedUsers
+//        }
+//    }
+//}
 //
 
 import Foundation
@@ -27,10 +70,8 @@ class UserData: ObservableObject {
     }
 
     func authenticate(username: String, password: String) -> Bool {
-        if let user = users.first(where: { $0.username == username && $0.password == password }) {
-            return true
-        }
-        return false
+        // Simplified authentication logic to eliminate unused 'user' variable
+        return users.contains(where: { $0.username == username && $0.password == password })
     }
 
     private func saveUsers() {
@@ -46,4 +87,3 @@ class UserData: ObservableObject {
         }
     }
 }
-
